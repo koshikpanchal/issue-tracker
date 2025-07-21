ba2dfc40d8ed40aeb3bcd814b46013be3a4ed214
Koshik Panchal <koshikpanchal1993@gmail.com>
2025-07-21 16:43:19 +0530

    add edit button and Icon pack

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,7 @@
 import { prisma } from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
-import React from "react";
-import IssueStatusBadge from "../components/IssueStatusBadge";
+import { IssueStatusBadge, Link } from "@/app/components/index";
 import IssueActions from "./IssueActions";
-import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
